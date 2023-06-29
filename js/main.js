fd0b8fae2ac5a2c9c79c426d6a060053e1e4b156
monitha <spiemgi@gmail.com>
2023-06-29 18:52:33 -0400

    entrega

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 
+cont parrafo = document.querySelector('#parrafo');
+
+parrafo.addEventListener('click', funtion(e) {
+e.target.classList.toggle('parrafo-modificado');
+});
 
 
 const menuItems = document.querySelectorAll('.menu-item');
@@ -7,14 +12,14 @@ meniItems.forEach(function(item) {
     item.addEventListener('click', funtion(e) {
         const currentItem = document.querySelector('.active');
         currentItem.classList.remove('active');
-        e.target.classList.add ('active');
+        e.target.classList.add('active');
     });
 }):
 
 
-const changeColor = document.querySelector('#changeColor');
+const changeColor = document.querySelector('#');
 changeColor.addEventListener('click', function() {
-    document.body.classList.toggle('changeColor')
+    document.body.classList.toggle('change-Color')
 })
 
 
